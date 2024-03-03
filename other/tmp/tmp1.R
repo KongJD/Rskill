8ad534a004526da83afa675896dc696f91fae26b
jindong <jindong123@qq.com>
2024-03-03 21:12:22 +0800

    tmp代码

--- other/tmp/tmp1.R
+++ other/tmp/tmp1.R
@@ -15,3 +15,34 @@ exprSet <- gtfdata %>%
   distinct(gene_name,.keep_all = T) %>%
   ## 删除多余列
   dplyr::select(-rowMean)
+
+
+metadata <- data.frame(TCGA_id=rownames(exprSet))
+
+for (i in 1:nrow(metadata)) {
+  ## 指示
+  print(i)
+
+  ## substring的用法，这是元素获取
+  num <- as.numeric(substring(metadata[i,1],14,15))
+
+  #如果是肿瘤，就给第2列加上Tumor
+  if (num %in% seq(1,9)) {
+    metadata[i,2] = "Tumor"
+  }
+
+  #如果是正常组织，就给第2列加上Normal
+  if (num %in% seq(10,29)) {
+    metadata[i,2] = "Normal"
+  }
+}
+## 修改名称
+colnames(metadata) <- c("TCGA_id","sample")
+
+
+
+
+
+
+
+
